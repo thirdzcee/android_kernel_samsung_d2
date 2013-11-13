9da309045cebb894f6a30eeb31ae8f478c038083
Jason Riordan <jriordan001@gmail.com>
2013-11-12 21:57:14 -0500

    VMIN bypass
    Vmin(): create new bypass config to disable vmin without disabling voltage control
    
    Signed-off-by: thirdzcee <thirdzcee@gmail.com>
    
    Conflicts:
    	arch/arm/configs/torched_elite_defconfig
    	arch/arm/mach-msm/Kconfig
    	arch/arm/mach-msm/acpuclock-krait.c
    
    (cherry picked from commit 6941bd5da97ff7d134b1167dea9c597354b3880f)
    Signed-off-by: thirdzcee <thirdzcee@gmail.com>
    
    Conflicts:
    	arch/arm/configs/cyanogen_d2dcm_defconfig
    	arch/arm/mach-msm/acpuclock-krait.c
    
    fixup vmin bypass properly
    
    (cherry picked from commit b3aaa0422cf912f5667b894fc6900bec2a8eadd0)
    Signed-off-by: thirdzcee <thirdzcee@gmail.com>
    (cherry picked from commit c4499cfaaa740f7283866835bfb23255378526d0)
    Signed-off-by: thirdzcee <thirdzcee@gmail.com>

--- arch/arm/mach-msm/acpuclock-krait.c
+++ arch/arm/mach-msm/acpuclock-krait.c
@@ -1042,14 +1042,18 @@ static struct notifier_block __cpuinitdata acpuclk_cpu_notifier = {
 
 static const int krait_needs_vmin(void)
 {
+#ifdef CONFIG_BYPASS_VMIN
+	return 0;
+#else
 	switch (read_cpuid_id()) {
-	case 0x511F04D0: /* KR28M2A20 */
+ 	case 0x511F04D0: /* KR28M2A20 */
 	case 0x511F04D1: /* KR28M2A21 */
 	case 0x510F06F0: /* KR28M4A10 */
 		return 1;
 	default:
 		return 0;
 	};
+#endif
 }
 
 static void krait_apply_vmin(struct acpu_level *tbl)
