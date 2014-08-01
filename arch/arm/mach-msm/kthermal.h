e8a31b82eccdb046f6895b755d91797115a15777
thirdzcee <thirdzcee@gmail.com>
2014-08-01 13:04:49 +0800

    Revert "Disable msm_thermal and now use kthermal plus update kthermal"
    This reverts commit a1ac1d8d08a321f7f23316e024be9addab91266e.

--- arch/arm/mach-msm/kthermal.h
+++ arch/arm/mach-msm/kthermal.h
@@ -7,32 +7,16 @@
 #include <linux/cpu.h>
 #include <linux/cpufreq.h>
 #include <linux/msm_tsens.h>
+#include <linux/msm_thermal.h>
 #include <linux/platform_device.h>
 #include <linux/of.h>
 #include <mach/cpufreq.h>
 
-struct kmsm_thermal_data {
-	uint32_t sensor_id;
-	int isthrottling;
-	uint32_t poll_speed;
-	uint32_t temp_limit_degC_start;
-	uint32_t temp_limit_degC_stop;
-	uint32_t freq_steps_while_throttling;
-	uint32_t minimum_throttle_mhz;
-	uint32_t core_limit_temp_degC;
-	uint32_t core_temp_hysteresis_degC;
-	uint32_t core_control_mask;
-	long current_temp;
-};
-
-static struct kmsm_thermal_data kmsm_thermal_info = {
+static struct msm_thermal_data msm_thermal_pdata = {
 	.sensor_id = 0,
-	.isthrottling = false,
-	.poll_speed = 1000,
-	.temp_limit_degC_start = 60,
-	.temp_limit_degC_stop = 55,
-	.freq_steps_while_throttling = 1,
-	.minimum_throttle_mhz = 972000,
-	.current_temp = 0,
+	.poll_ms = 1000,
+	.limit_temp_degC = 50,
+	.temp_hysteresis_degC = 10,
+	.freq_step = 2,
 };
 
