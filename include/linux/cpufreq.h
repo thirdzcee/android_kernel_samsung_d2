7357a35f84f19143f657ecb6fbf9288394467cd3
thirdzcee <thirdzcee@gmail.com>
2014-08-06 03:32:06 +0800

    add hybrid governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -373,6 +373,9 @@ extern struct cpufreq_governor cpufreq_gov_ondemand;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
 extern struct cpufreq_governor cpufreq_gov_intellidemand;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intellidemand)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HYBRID)
+extern struct cpufreq_governor cpufreq_gov_hybrid;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_hybrid)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CONSERVATIVE)
 extern struct cpufreq_governor cpufreq_gov_conservative;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_conservative)
