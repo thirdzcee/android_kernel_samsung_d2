da933265acde172fb810b7d6ed6b5f1d2ae5a487
thirdzcee <thirdzcee@gmail.com>
2014-09-24 01:29:18 +0800

    tune freq & temp parameters to my liking
    Signed-off-by: thirdzcee <thirdzcee@gmail.com>

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -30,16 +30,16 @@ static uint32_t limited_max_freq = MSM_CPUFREQ_NO_LIMIT;
 #define DEF_SHUTDOWNTEMP 80
 
 //max thermal limit
-#define DEF_ALLOWED_MAX_HIGH 76
+#define DEF_ALLOWED_MAX_HIGH 75
 #define DEF_ALLOWED_MAX_FREQ 384000
 
 //mid thermal limit
-#define DEF_ALLOWED_MID_HIGH 72
-#define DEF_ALLOWED_MID_FREQ 648000
+#define DEF_ALLOWED_MID_HIGH 70
+#define DEF_ALLOWED_MID_FREQ 702000
 
 //low thermal limit
-#define DEF_ALLOWED_LOW_HIGH 70
-#define DEF_ALLOWED_LOW_FREQ 972000
+#define DEF_ALLOWED_LOW_HIGH 65
+#define DEF_ALLOWED_LOW_FREQ 1242000
 
 //Sampling interval
 #define DEF_THERMAL_CHECK_MS 1000
